5a7e53b7dc7300b4ed57942182bb6d20c9d19bda
DAVIDMA0808 <jianxiang.ma@mail.utoronto.ca>
2023-08-30 11:55:06 -0400

    added message for registration if it's successful.

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -18,7 +18,7 @@ function Registration() {
 
     const onSubmit = (data) => {
         axios.post("http://localhost:3001/auth", data).then(() => {
-            console.log(data);
+            alert("User registration successful :)");
         });
     }
 
